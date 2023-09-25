851b1d46ce50f658bef534d29015d9ae9f5ce0d1
Matias Figueroa <matias.figueroa50@inacapmail.cl>
2023-09-25 15:20:18 -0300

    merge completado completado

--- MRFS_app1/views.py
+++ MRFS_app1/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse
 
 def vista1(request):
     html="""
-
+    <h1 style="color:blue">BIENVENIDO</h1>
+    <h1> ESTÁS EN LA VISTA 1 </h1>
+    <a href="/vista2/" >Ir a vista 2</a>
+    <a href="http://127.0.0.1:8000/vista3/" >Ir a vista 3</a>
+    <a href="http://127.0.0.1:8000/vista4/" >Ir a vista 4</a>
 
 """
 
@@ -12,7 +16,11 @@ def vista1(request):
 
 def vista2(request):
     html="""
-<h1>AAAA</h1>
+    <h1 style="color:brown">BIENVENIDO</h1>
+    <h1> ESTÁS EN LA VISTA 2 </h1>
+    <a href="/" >Ir a vista 1</a>
+    <a href="http://127.0.0.1:8000/vista3/" >Ir a vista 3</a>
+    <a href="http://127.0.0.1:8000/vista4/" >Ir a vista 4</a>
 
 """
 
--- MRFS_app2/views.py
+++ MRFS_app2/views.py
@@ -6,8 +6,8 @@ def vista3(request):
     html="""
     <h1 style="color:green">BIENVENIDO</h1>
     <h1> ESTÁS EN LA VISTA 3 </h1>
-    <a href="" >Ir a vista 1</a>
-    <a href="" >Ir a vista 2</a>
+    <a href="http://127.0.0.1:8000/" >Ir a vista 1</a>
+    <a href="http://127.0.0.1:8000/vista2/" >Ir a vista 2</a>
     <a href="/vista4/" >Ir a vista 4</a>
 """
 
@@ -17,8 +17,8 @@ def vista4(request):
     html="""
     <h1 style="color:red">BIENVENIDO</h1>
     <h1> ESTÁS EN LA VISTA 4 </h1>
-    <a href="" >Ir a vista 1</a>
-    <a href="" >Ir a vista 2</a>
+    <a href="http://127.0.0.1:8000/" >Ir a vista 1</a>
+    <a href="http://127.0.0.1:8000/vista2/" >Ir a vista 2</a>
     <a href="/vista3/" >Ir a vista 3</a>
 """
 
